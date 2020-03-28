0cf48343a0ce243c004c21f3cf422b52df52d019
Nitish Sinha <nitishsinha@tracxn.com>
2020-03-29 01:33:53 +0530

    Updates

---
+++ precache-manifest.e771b19ceedad2fe81c4b2baa41d756e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16ec52d57bc763079d8bab6508366a30",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ed6062ee2b358a31fa1e",
+    "url": "/static/css/main.ab68c586.chunk.css"
+  },
+  {
+    "revision": "dff3191fd663387ecefc",
+    "url": "/static/js/2.bc7af0c9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.bc7af0c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed6062ee2b358a31fa1e",
+    "url": "/static/js/main.f50aeb8f.chunk.js"
+  },
+  {
+    "revision": "1d60461088ba88d22d28",
+    "url": "/static/js/runtime-main.39a15984.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- src/components/Hello.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-class Hello extends React.component {
-  render() {
-    return (<div>Hello Brother!!</div>);
-  }
-}
-export default Hello;
