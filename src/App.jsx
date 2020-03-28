cb2f8f613a1b128c257bc8e12dde17b6d63e2754
Nitish Sinha <nitishsinha@tracxn.com>
2020-03-29 01:59:03 +0530

    testing latest push change to git pages

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,41 @@ import logo from './logo.svg';
 import Game from './components/tic-tac-toe/Game';
 import './common/css/App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Game />
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      gameOn: false,
+    };
+  }
+
+  openGame = () => {
+    this.setState({
+      gameOn: true,
+    });
+  }
+
+  render() {
+    const { gameOn } = this.state;
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          {
+              gameOn
+                ? (<Game />)
+                : (
+                  <div>
+                    {' '}
+                    Welcome!!!
+                    <div className="App-link cursor-pointer" role="presentation" onClick={() => this.openGame()} onKeyPress={() => {}}>Lets play a game.</div>
+                  </div>
+                )
+          }
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
